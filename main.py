114ad309f04ad676e6363ce9af78d9b79b4cf7f5
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-20 22:23:59 -0700

    add reasons for invalid input

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 import inquirer
 import pymysql
 from rich import print
@@ -14,8 +13,9 @@ if intent == "Log In":
     creds = inquirer.prompt(login)
 elif intent == "Register":
     info = inquirer.prompt(register)
-    crsr.execute("INSERT INTO user_creds (username, password) VALUES (%s, %s)", (info["username"], info["password"]))
-    id = crsr.lastrowid
-    crsr.execute("INSERT INTO user_info (id, fname, lname) VALUES (%s, %s, %s)", (id, info["fname"], info["lname"]))
+    if info:
+        crsr.execute("INSERT INTO user_creds (username, password) VALUES (%s, %s)", (info["username"], info["password"]))
+        id = crsr.lastrowid
+        crsr.execute("INSERT INTO user_info (id, fname, lname) VALUES (%s, %s, %s)", (id, info["fname"], info["lname"]))
 else:
     exit()
--- validation.py
+++ validation.py
@@ -1,8 +1,35 @@
 from inquirer import errors
+import pymysql
+
+db = pymysql.connect(host="localhost", user="root", password="1052", database="student_tracker")
+crsr = db.cursor()
 
 def validateUsername(answers, username):
-    if len(username) < 3:
-        raise errors.ValidationError('Username must be at least 3 characters.')
+    if len(username) <= 3:
+        raise errors.ValidationError(username, reason='Username must be at least 4 characters.')
     elif len(username) > 64:
-        raise errors.ValidationError('Username must be at most 64 characters.')
+        raise errors.ValidationError(username, reason='Username must be at most 64 characters.')
+    return True
+
+def validateNewUsername(answers, username):
+    if len(username) <= 3:
+        raise errors.ValidationError(username, reason='Username must be at least 4 characters.')
+    elif len(username) > 64:
+        raise errors.ValidationError(username, reason='Username must be at most 64 characters.')
+    elif crsr.execute("SELECT username FROM user_creds where username=%s", (username)):
+        raise errors.ValidationError(username, reason='Username already exists.')
+    return True
+
+def validatePassword(answers, password):
+    if len(password) < 8:
+        raise errors.ValidationError(password, reason='Password must be at least 8 characters.')
+    elif len(password) > 64:
+        raise errors.ValidationError(password, reason='Password must be at most 64 characters.')
+    return True
+
+def validateName(answers, name):
+    if len(name) < 1:
+        raise errors.ValidationError(name, reason='Name must be at least 1 character.')
+    elif len(name) > 64:
+        raise errors.ValidationError(name, reason='Name must be at most 64 characters.')
     return True
