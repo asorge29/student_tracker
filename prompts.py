755534e38afb29f93c6b8762b3dd87a9cedde8d1
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-20 22:23:36 -0700

    add validation

--- prompts.py
+++ prompts.py
@@ -3,12 +3,12 @@ from validation import *
 
 login = [
     inquirer.Text('username', message='Enter your username', validate=validateUsername),
-    inquirer.Password('password', message='Enter your password')
+    inquirer.Password('password', message='Enter your password', validate=validatePassword),
 ]
 
 register = [
-    inquirer.Text('username', message='Create a username', validate=validateUsername),
-    inquirer.Password('password', message='Create a password'),
-    inquirer.Text('fname', message='Enter your first name'),
-    inquirer.Text('lname', message='Enter your last name'),
+    inquirer.Text('username', message='Create a username', validate=validateNewUsername),
+    inquirer.Password('password', message='Create a password', validate=validatePassword),
+    inquirer.Text('fname', message='Enter your first name', validate=validateName),
+    inquirer.Text('lname', message='Enter your last name', validate=validateName),
 ]
