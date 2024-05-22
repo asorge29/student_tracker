c6fdb5fb05ec0ed2e3a77a370ea0dea59848b973
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-22 07:44:19 -0700

    main menu

--- prompts.py
+++ prompts.py
@@ -8,7 +8,11 @@ login = [
 
 register = [
     inquirer.Text('username', message='Create a username', validate=validateNewUsername),
-    inquirer.Password('password', message='Create a password', validate=validatePassword),
+    inquirer.Password('password', message='Create a password', validate=validateNewPassword),
     inquirer.Text('fname', message='Enter your first name', validate=validateName),
     inquirer.Text('lname', message='Enter your last name', validate=validateName),
+]
+
+mainMenu = [
+    inquirer.List('mainMenu', message='What do you want to do?', choices=['View Classes & Grades', 'Update Classes & Grades', 'Export Transcript', 'Calculate GPA', 'Update User Info', 'Exit']),
 ]
