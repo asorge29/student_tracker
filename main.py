4db048833bbba2c3a0e0af5a667309ccce5a2de6
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-22 07:43:45 -0700

    main menu

--- main.py
+++ main.py
@@ -2,15 +2,43 @@ import inquirer
 import pymysql
 from rich import print
 from prompts import *
+from tkinter.filedialog import asksaveasfilename
+from os import system
 
-db = pymysql.connect(host="localhost", user="root", password="1052", database="student_tracker")
+db = pymysql.connect(host="localhost", user="root", password="399764abc", database="student_tracker")
 db.autocommit(True)
 crsr = db.cursor()
 
-intent = inquirer.list_input("What do you want to do?", choices=["Log In", "Register", "Exit"])
+def saveTranscript(transcript):
+    file = asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+    if file:
+        with open(file, "w") as f:
+            f.write(transcript)
+
+def clearTerm():
+    if system == "Windows":
+        system("cls")
+    else:
+        system("clear")
+
+intent = inquirer.list_input("What do you want to do? (Select with arrow keys)", choices=["Log In", "Register", "Exit"])
+clearTerm()
 
 if intent == "Log In":
     creds = inquirer.prompt(login)
+    crsr.execute("select fname, lname from user_info where id=(select id from user_creds where username=%s)", (creds["username"]))
+    fname, lname = crsr.fetchone()
+    clearTerm()
+    choice = {"mainMenu":str}
+    while choice['mainMenu'] != "Exit":
+        clearTerm()
+        print(f"[green]Welcome back, {fname}!")
+        choice = inquirer.prompt(mainMenu)
+        print(f"[green]Your choice:[/green] [red][underline]{choice['mainMenu']}")
+    clearTerm()
+    print("[red]Exiting...")
+    print("[green]Goodbye!")
+    exit()
 elif intent == "Register":
     info = inquirer.prompt(register)
     if info:
