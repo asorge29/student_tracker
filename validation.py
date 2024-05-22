5b7fcb2e607072bd3f89382475d96c6d0af32917
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-22 07:44:37 -0700

    password validation

--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 from inquirer import errors
 import pymysql
 
-db = pymysql.connect(host="localhost", user="root", password="1052", database="student_tracker")
+db = pymysql.connect(host="localhost", user="root", password="399764abc", database="student_tracker")
 crsr = db.cursor()
 
 def validateUsername(answers, username):
@@ -9,6 +9,8 @@ def validateUsername(answers, username):
         raise errors.ValidationError(username, reason='Username must be at least 4 characters.')
     elif len(username) > 64:
         raise errors.ValidationError(username, reason='Username must be at most 64 characters.')
+    elif not crsr.execute("SELECT username FROM user_creds where username=%s", (username)):
+        raise errors.ValidationError(username, reason='User not found.')
     return True
 
 def validateNewUsername(answers, username):
@@ -20,11 +22,20 @@ def validateNewUsername(answers, username):
         raise errors.ValidationError(username, reason='Username already exists.')
     return True
 
+def validateNewPassword(answers, password):
+    if len(password) < 8:
+        raise errors.ValidationError(password, reason='Password must be at least 8 characters.')
+    elif len(password) > 64:
+        raise errors.ValidationError(password, reason='Password must be at most 64 characters.')
+    return True
+
 def validatePassword(answers, password):
     if len(password) < 8:
         raise errors.ValidationError(password, reason='Password must be at least 8 characters.')
     elif len(password) > 64:
         raise errors.ValidationError(password, reason='Password must be at most 64 characters.')
+    elif not crsr.execute(f"SELECT password FROM user_creds where password=%s and username=%s", (password, answers["username"])):
+        raise errors.ValidationError(password, reason='Incorrect password.')
     return True
 
 def validateName(answers, name):
